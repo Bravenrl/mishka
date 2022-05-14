47f51fe41df94e9f037da0366af76e1e00fbef5f
Bravenrl <bravenrl@gmail.com>
2022-05-14 04:14:44 +0300

    setup gulp & stylelint

--- gulpfile.js
+++ gulpfile.js
@@ -1,48 +1,61 @@
-import gulp from "gulp";
+import gulp from 'gulp';
 import plumber from 'gulp-plumber';
 import dartSass from 'sass';
 import gulpSass from 'gulp-sass';
 import postcss from 'gulp-postcss';
 import autoprefixer from 'autoprefixer';
-import browserSync from "browser-sync";
+import browserSync from 'browser-sync';
+import gulpStylelint from 'gulp-stylelint';
+import cleanCSS from 'gulp-clean-css';
+import rename from 'gulp-rename'
+
 
 const sass = gulpSass(dartSass);
-const browser = browserSync.create()
+const browser = browserSync.create();
+
 // Styles
 
 export const styles = () => {
-  return gulp.src('source/sass/style.scss', { sourcemaps: true })
+  return gulp
+    .src('source/scss/style.scss', { sourcemaps: true })
     .pipe(plumber())
     .pipe(sass())
-    .pipe(postcss([
-      autoprefixer()
-    ]))
+    .pipe(postcss([autoprefixer()]))
+    .pipe(cleanCSS())
+    .pipe(rename({suffix: '.min'}))
     .pipe(gulp.dest('source/css', { sourcemaps: '.' }))
     .pipe(browser.stream());
-}
+};
+
+// Stylelint
+
+const lintCss = () => {
+  return gulp.src('source/scss/**/*.scss').pipe(
+    gulpStylelint({
+      reporters: [{ formatter: 'string', console: true }],
+    })
+  );
+};
 
 // Server
 
-const server = (done) => {
+const server = done => {
   browser.init({
     server: {
-      baseDir: 'source'
+      baseDir: 'source',
     },
     cors: true,
     notify: false,
     ui: false,
   });
   done();
-}
+};
 
 // Watcher
 
 const watcher = () => {
-  gulp.watch('source/sass/**/*.scss', gulp.series(styles));
+  gulp.watch('source/scss/**/*.scss', gulp.series(styles));
   gulp.watch('source/*.html').on('change', browser.reload);
-}
+};
 
-
-export default gulp.series(
-  styles, server, watcher
-);
+export default gulp.series(styles, lintCss, server, watcher);
